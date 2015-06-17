f28b679af2506700519940541ee63446f69fa0f9
luketis <luk.ties@gmail.com>
2015-06-17 10:26:09 -0300

    mais

--- .#grafico.c
+++
@@ -1,1 +0,0 @@
-luke@luke-Inspiron-7537.2409:1434493668
--- grafico.c
+++ grafico.c
@@ -43,8 +43,7 @@ void desenha_grid(WINDOW* win, Map mapa, PIC texturas[], coord tamanhotela, int 
   Boat embarcoes[256];
   int desenhado[mapa->m][mapa->n];
   int i, j, k = 0;
-  PIC p;
-  MASK msk;
+  
   for(i = 0; i < mapa->m; i++)
     for(j = 0; j < mapa->n; j++)
       desenhado[i][j] = 0;
@@ -58,19 +57,58 @@ void desenha_grid(WINDOW* win, Map mapa, PIC texturas[], coord tamanhotela, int 
 	if(ehembarcacao(mapa->map[i][j])){
 	  switch(mapa->map[i][j]){
 	  case 'S':
-	    
+	    embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 0, texturas[5]);
 	    break;
 	  case 'P':
-	    
+	    if(i + 1 < mapa->m && j - 1 >= 0 && mapa->map[i + 1][j - 1] == 'P')
+	      embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 0, texturas);
+	    //usa do 6-8 (incl)
+	    else if(i + 1 < mapa->m && mapa->map[i + 1][j] == 'P')
+	      embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 1, texturas);
+	    //usa o 9
+	    else if(i + 1 < mapa->m && j + 1 < mapa->n && mapa->map[i + 1][j + 1] == 'P')
+	      embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 2, texturas);
+	    //usa do 10-12(incl)
+	    else if(j + 1 < mapa->n && mapa->map[i][j + 1] == 'P')
+	      embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 3, texturas);
+	    //usa o 13
 	    break;
 	  case 'C':
-	    
+	    if(i + 1 < mapa->m && j - 1 >= 0 && mapa->map[i + 1][j - 1] == 'C')
+	      embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 0, texturas);
+	    //usa do 6-8 (incl)
+	    else if(i + 1 < mapa->m && mapa->map[i + 1][j] == 'C')
+	      embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 1, texturas);
+	    //usa o 14
+	    else if(i + 1 < mapa->m && j + 1 < mapa->n && mapa->map[i + 1][j + 1] == 'C')
+	      embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 2, texturas);
+	     //usa do 10-12(incl)
+	    else if(j + 1 < mapa->n && mapa->map[i][j + 1] == 'C')
+	      embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 3, texturas);
+	    //usa o 15
 	    break;
 	  case 'D':
+	    if(i + 1 < mapa->m && j - 1 >= 0 && mapa->map[i + 1][j - 1] == 'D')
+	      embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 0, texturas[16]);
+	    else if(i + 1 < mapa->m && mapa->map[i + 1][j] == 'D')
+	      embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 1, texturas[17]);
+	    else if(i + 1 < mapa->m && j + 1 < mapa->n && mapa->map[i + 1][j + 1] == 'D')
+	      embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 2, texturas[18]);
+	    else if(j + 1 < mapa->n && mapa->map[i][j + 1] == 'D')
+	      embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 3, texturas[19]);
 	    
 	    break;
 	  case 'H':
-	    
+	    if(i + 2 < mapa->m && mapa->map[i + 2][j] == 'H')
+	      if(j - 1 >= 0 && mapa->map[i + 1][j - 1] == 'H')
+		embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 0, texturas[18]);
+	      else
+		embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 1, texturas[19]);
+	    else
+	      if(j - 1 >= 0 && mapa->map[i + 1][j - 1] == 'H')
+		embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 2, texturas[20]);
+	      else
+		embarcacoes[k] = adesenhar_S(win, co, mapa, desenhado, tamanhocelula, 3, texturas[21]);
 	    break;
 	  }
 	  k++;
@@ -84,7 +122,7 @@ void desenha_grid(WINDOW* win, Map mapa, PIC texturas[], coord tamanhotela, int 
       }
   
   for(i = 0; i < k; i++);
-  
+  //TODO
 
 }
 
@@ -97,19 +135,19 @@ void desenha_celula(WINDOW* win, Map mapa, coord coordenadas, PIC texturas[], co
   switch(mapa->map[coordenadas.x][coordenadas.y]){
     
   case ' ':
-    p = ReadPic(win, "texturas/1.xpm", msk);
+    p = texturas[0];
     break;
   case '=':
-    p = ReadPic(win, "texturas/2.xpm", msk);
+    p = texturas[1];
     break;
   case '*':
-    p = ReadPic(win, "texturas/3.xpm", msk);
+    p = texturas[2];
     break;
   case '!':
-    p = ReadPic(win, "texturas/4.xpm", msk); 
+    p = texturas[3]; 
     break;
   case '+':
-    p = ReadPic(win, "texturas/5.xpm", msk);
+    p = texturas[4];
     break;
 
   }
@@ -118,3 +156,12 @@ void desenha_celula(WINDOW* win, Map mapa, coord coordenadas, PIC texturas[], co
 
 }
 
+Boat adesenhar_S(WINDOW* win, coord coordenas, Map mapa, int** desenhado, int tamanhocelula, int tipo, PIC textura);
+
+Boat adesenhar_P(WINDOW* win, coord coordenas, Map mapa, int** desenhado, int tamanhocelula, int tipo, PIC texturas);
+
+Boat adesenhar_C(WINDOW* win, coord coordenas, Map mapa, int** desenhado, int tamanhocelula, int tipo, PIC texturas);
+
+Boat adesenhar_D(WINDOW* win, coord coordenas, Map mapa, int** desenhado, int tamanhocelula, int tipo, PIC textura);
+
+Boat adesenhar_H(WINDOW* win, coord coordenas, Map mapa, int** desenhado, int tamanhocelula, int tipo, PIC textura);
